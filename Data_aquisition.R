51167baa0603ab727078302ef34fafa0bcf69868
Hannahmagda <hannahschweren@gmail.com>
2024-01-18 17:59:56 +0100

    restructuring

---
+++ Data_aquisition.R
@@ -0,0 +1,147 @@
+
+
+############# Data aquisition ###############################################################
+
+
+
+#load necessary packages
+library(legislatoR)
+library(WikipediR)
+library(rvest)
+library(dplyr)
+
+
+
+#load legislatoR Core data for each country and filter for politicians that are alive
+
+# fr_core <-  get_core((legislature = "fra"))
+# fr_core_alive <- fr_core %>% filter(is.na(death))
+
+# deu_core <- get_core(legislature = "deu")
+# deu_core_alive <- deu_core %>% filter(is.na(death))
+
+# aut_core <- get_core(legislature = "aut")
+# aut_core_alive <- aut_core %>% filter(is.na(death))
+
+# can_core <- get_core(legislature = "can")
+# can_core_alive <- can_core %>% filter(is.na(death))
+
+# cze_core <- get_core(legislature = "cze")
+# cze_core_alive <- cze_core %>% filter(is.na(death))
+
+# esp_core <- get_core(legislature = "esp")
+# esp_core_alive <- esp_core %>% filter(is.na(death))
+
+# irl_core <- get_core(legislature = "irl")
+# irl_core_alive <- irl_core %>% filter(is.na(death))
+
+# sco_core <- get_core(legislature = "sco")
+# sco_core_alive <- sco_core %>% filter(is.na(death))
+
+# gbr_core <- get_core(legislature = "gbr")
+# gbr_core_alive <- gbr_core %>% filter(is.na(death))
+
+# usa_house <- get_core(legislature = "usa_house")
+# usa_senate <- get_core(legislature = "usa_senate")
+# 
+# usa_core <- bind_rows(usa_house, usa_senate)
+# usa_core_alive <- usa_core %>% filter(is.na(death))
+
+
+########### Functions #######################################################################
+
+###text aquisition german
+
+de_text_pipeline <- function(page_name) {
+  Sys.sleep(runif(1, 1, 2))
+  
+  # Check if page_name is missing
+  if (is.na(page_name) || page_name == "") {
+    return("No Wikipedia page name provided or missing.")
+  }
+  
+  # Try fetching Wikipedia content
+  tryCatch({
+    wp_content <- WikipediR::page_content("de", "wikipedia", page_name = page_name)
+    plain_text <- html_text(read_html(wp_content$parse$text$`*`))
+    return(plain_text)
+  }, error = function(e) {
+    return(paste("Error fetching content for page:", page_name))
+  })
+}
+
+########### functions ###################################################################################
+
+
+### function for wiki text aquisition, switch language to desired language per country eg. fr, es, en etc.
+
+de_text_pipeline <- function(page_name) {
+  Sys.sleep(runif(1, 1, 2))
+  
+  # Check if page_name is missing
+  if (is.na(page_name) || page_name == "") {
+    return("No Wikipedia page name provided or missing.")
+  }
+  
+  # Try fetching Wikipedia content
+  tryCatch({
+    wp_content <- WikipediR::page_content("de", "wikipedia", page_name = page_name)
+    plain_text <- html_text(read_html(wp_content$parse$text$`*`))
+    return(plain_text)
+  }, error = function(e) {
+    return(paste("Error fetching content for page:", page_name))
+  })
+}
+
+######################################################################################################
+
+# #iterate over dataset, once for each country and save the dataset containing the wikipedia text
+
+# deu_alive_text <- deu_core_alive %>%
+#   mutate(plain_text = sapply(wikititle, de_text_pipeline))
+#save
+#write.csv(deu_alive_text, file = "raw_data/deu_alive_text.csv")
+
+
+# aut_alive_text <- aut_core_alive %>%
+#   mutate(plain_text = sapply(wikititle, de_text_pipeline))
+# write.csv(aut_alive_text, file = "raw_data/aut_alive_text.csv")
+
+
+# esp_alive_text <- esp_core_alive %>%
+#    mutate(plain_text = sapply(wikititle, esp_text_pipeline))
+#  write.csv(esp_alive_text, file = "raw_data/esp_alive_text.csv")
+
+
+#  gbr_alive_text <- gbr_core_alive %>%
+#    mutate(plain_text = sapply(wikititle, en_text_pipeline))
+#  write.csv(gbr_alive_text, file = "raw_data/gbr_alive_text.csv")
+
+
+#  irl_alive_text <- irl_core_alive %>%
+#    mutate(plain_text = sapply(wikititle, en_text_pipeline))
+#  write.csv(irl_alive_text, file = "raw_data/irl_alive_text.csv")
+
+#  
+# can_alive_text <- can_core_alive %>%
+#    mutate(plain_text = sapply(wikititle, en_text_pipeline))
+#  write.csv(can_alive_text, file = "raw_data/can_alive_text.csv")
+#  
+#  usa_alive_text <- usa_core_alive %>%
+#    mutate(plain_text = sapply(wikititle, en_text_pipeline))
+#  write.csv(usa_alive_text, file = "raw_data/usa_alive_text.csv")
+
+#  sco_alive_text <- sco_core_alive %>%
+#   mutate(plain_text = sapply(wikititle, en_text_pipeline))
+# write.csv(sco_alive_text, file = "raw_data/sco_alive_text.csv")
+
+
+#  cze_alive_text <- cze_core_alive %>%
+#   mutate(plain_text = sapply(wikititle, sze_text_pipeline))
+# write.csv(cze_alive_text, file = "raw_data/cze_alive_text.csv")
+
+
+# fr_alive_text <- fr_core_alive %>%
+#   mutate(plain_text = sapply(wikititle, fra_text_pipeline))
+# write.csv(fr_alive_text, file = "raw_data/fr_alive_text.csv")
+
---
+++ data_cleaning.R
@@ -0,0 +1,123 @@
+
+############# Data cleaning ###############################################################
+
+
+
+#load necessary packages
+
+library(rvest)
+library(dplyr)
+library(stringr) #für word count
+library(ggplot2)
+
+
+
+
+#import raw data
+
+cze_alive_text <- read_csv("raw_data/cze_alive_text.csv")
+
+deu_alive_text <- read_csv("raw_data/deu_alive_text.csv")
+
+fr_alive_text <- read_csv("raw_data/fr_alive_text.csv")
+
+usa_alive_text <- read_csv("raw_data/usa_alive_text.csv")
+
+gbr_alive_text <- read_csv("raw_data/gbr_alive_text.csv")
+
+sco_alive_text <- read_csv("raw_data/sco_alive_text.csv")
+
+irl_alive_text <- read_csv("raw_data/irl_alive_text.csv")
+
+esp_alive_text <- read_csv("raw_data/esp_alive_text.csv")
+
+can_alive_text <- read_csv("raw_data/can_alive_text.csv")
+
+aut_alive_text <- read_csv("raw_data/aut_alive_text.csv")
+
+
+cze_alive_text$Word_Count <- sapply(cze_alive_text$plain_text, count_words)
+
+
+
+######### functions ##################################################################
+
+
+
+#word count
+count_words <- function(text) {
+  words <- str_extract_all(text, "\\b\\w+\\b")[[1]]
+  return(length(words))
+}
+
+
+count_words <- function(text) {
+  words <- str_extract_all(text, "\\b\\w+\\b")[[1]]
+  return(length(words))
+}
+
+
+#clean data
+
+clean_data <- function(df) {
+  df$plain_text <- str_remove_all(df$plain_text, "\\..*?\\{.*?\\}")
+  df <- df %>%
+    filter(!grepl("^Redirect to:", plain_text, ignore.case = TRUE) &
+             !grepl("may refer to:", plain_text, ignore.case = TRUE))
+  # Add more cleaning steps if needed
+  
+  return(df)
+}
+
+clean_data <- function(df) {
+  initial_rows <- nrow(df)
+  
+  # Remove CSS-like structures
+  df$plain_text <- str_remove_all(df$plain_text, "\\..*?\\{.*?\\}")
+  
+  # Initialize counters for removal reasons
+  removal_reason_redirect <- sum(grepl("^(Redirect to:|Weiterleitung nach:|Rediriger vers:|Redirige a:|Přesměrování na:)", df$plain_text, ignore.case = TRUE))
+  removal_reason_refering_page <- sum(grepl("may refer to:|ist der Name folgender Personen:|Cette page d'homonymie répertorie différentes personnes|může být:", df$plain_text, ignore.case = TRUE))
+  removal_reason_not_found <- sum(grepl("^(Error fetching content for page:|No Wikipedia page name provided or missing)", df$plain_text, ignore.case = TRUE))
+  
+  
+  # Filter rows based on specific conditions
+  df <- df %>%
+    filter(!grepl("^(Redirect to:|Weiterleitung nach:|Rediriger vers:|Redirige a:|Přesměrování na:)", plain_text, ignore.case = TRUE) &
+             !grepl("may refer to:|ist der Name folgender Personen:|Cette page d'homonymie répertorie différentes personnes|může být:", plain_text, ignore.case = TRUE) &
+             !grepl("Error fetching content for page:|No Wikipedia page name provided or missing", plain_text, ignore.case = TRUE))
+  
+  # Calculate the number of rows removed
+  rows_removed <- initial_rows - nrow(df)
+  
+  # Print statistics about the removal reasons
+  cat("Removal reasons:\n")
+  cat("  - Redirect:", removal_reason_redirect, "\n")
+  cat("  - Reference Page:", removal_reason_refering_page, "\n")
+  cat("  - Not Found/no name_provided:", removal_reason_not_found, "\n")
+  
+  
+  # Create a message about the cleaning process
+  cat("Cleaned data: Removed", rows_removed, "rows.\n")
+  
+  # Return the cleaned data frame
+  return(df)
+}
+
+
+###########################################################################################
+
+
+#clean the dataframes for each country
+
+cze <- clean_data(cze_alive_text)
+fra <- clean_data(fr_alive_text)
+deu <- clean_data(deu_alive_text)
+usa <- clean_data(usa_alive_text)
+gbr <- clean_data(gbr_alive_text)
+irl <- clean_data(irl_alive_text)
+sco <- clean_data(sco_alive_text)
+esp <- clean_data(esp_alive_text)
+aut <- clean_data(aut_alive_text)
+can <- clean_data(can_alive_text)
+ 
