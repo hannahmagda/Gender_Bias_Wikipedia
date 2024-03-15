5a0b396651a43cca345fa768a2398d78908beff3
Hannahmagda <hannahschweren@gmail.com>
2024-03-15 15:24:26 +0100

    new structure and sorted code

--- old_versions.R
+++ old_versions.R
@@ -3,53 +3,6 @@ library(jsonlite)
 library(dplyr)
 
 
-
-############################################################################################
-
-
-# das klappt auf deutsch für adolf arndt
-
-wikititle <- "Alois_Graf_von_Waldburg-Zeil"
-ten_years_ago <- format(Sys.Date() - 3650, "%Y%m%d%H%M%S")
-
-url <- paste0("https://de.wikipedia.org/w/api.php?action=query&prop=revisions&titles=", 
-              wikititle, "&rvlimit=1&rvdir=older&rvstart=", ten_years_ago, 
-              "&format=json&formatversion=2")
-
-response <- GET(url)
-response_content <- content(response, "text", encoding = "UTF-8")
-parsed_content <- fromJSON(response_content)
-
-# Extrahiere die revisions_id
-revision_id <- parsed_content$query$pages$revisions[[1]]$revid
-
-content_url <- paste0("https://de.wikipedia.org/w/api.php?action=parse&oldid=", 
-                      revision_id, "&format=json&prop=text")
-
-content_response <- GET(content_url)
-content_response_content <- content(content_response, "text", encoding = "UTF-8")
-parsed_content_response <- fromJSON(content_response_content)
-
-# Extrahiere den HTML-Inhalt
-html_content <- parsed_content_response$parse$text$`*`
-
-print(html_content)
-
-############################################################################################
-##plain text 
-
-# Lade den HTML-Inhalt in ein HTML-Dokument
-html_doc <- read_html(html_content)
-
-# Extrahiere den reinen Text
-plain_text <- html_text(html_doc)
-
-# Gib den reinen Text aus
-print(plain_text)
-
-
-
-
 ##################################### funktion
 
 library(httr)
